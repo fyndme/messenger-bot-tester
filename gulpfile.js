e1a61695424a1ee37bb3b7dd7349ae6671ff420f
Aziz Alghunaim <azizkag@gmail.com>
2017-06-03 03:42:23 -0700

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const ts = require( 'gulp-typescript');
+
+// pull in the project TypeScript config
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('compile', () => {
+  const tsResult = tsProject.src()
+  .pipe(tsProject());
+  return tsResult.js.pipe(gulp.dest('lib'));
+});
+
+gulp.task('watch', ['compile'], () => {
+  gulp.watch(['*.ts'], ['compile']);
+});
+
+gulp.task('default', ['watch']);
